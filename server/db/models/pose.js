f6d0cac289385b5e1abcf5c6550f16bc2e22e04a
emmapejko <emma.pejko@gmail.com>
2021-10-16 15:23:32 -0500

    feat: database set up

---
+++ server/db/models/pose.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+
+const Pose = {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  sanskrit: {
+    type: Sequelize.STRING
+  },
+  demo: {
+    type: Sequelize.STRING
+  },
+  difficulty: {
+    type: Sequelize.STRING
+  }
+}
+
+module.exports = Pose;
