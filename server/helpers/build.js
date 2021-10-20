4982a7571273d28251f13183154ce05d383e8ebb
emmapejko <emma.pejko@gmail.com>
2021-10-20 16:08:25 -0500

    fix: moved all images to aws s3 bucket

--- server/helpers/build.js
+++ server/helpers/build.js
@@ -1,7 +1,17 @@
 const { Pose, AfterPose, PoseBodyPart, User, UserPose, Flow, BodyPart, PoseFlow } = require('../db/sequelize');
 
-const build = (length, bodyparts, user_id) => {
-  return Pose.findOne({ where : { name: 'Cow Face'}});
+const build = async (length, bodyparts, user_id) => {
+  // create the flow
+  //const flow = await Flow.create({ name: `${user_id}`, length: length });
+
+  // bps: array of objects (bodyparts) { id: #, name: name }
+  const bps = Promise.all(bodyparts.map((part) => BodyPart.findOne({ where: { name: part }})));
+
+  // now get all poses that have an associate with bps[i].id
+  //const possiblePosesRows = Promise.all((await bps).map(bp => PoseBodyPart.findAll({ where: { }})));
+
+  //return Promise.all()
+ return Pose.findOne({ where : { name: 'Flying Splits'}});
 }
 
 
