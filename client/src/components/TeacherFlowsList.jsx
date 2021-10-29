651f449d391504c3e2a8f7875eb34d5336d45b01
Luke Johnson <benjaminlukejohnson@gmail.com>
2021-10-29 10:54:06 -0500

    adding deleted files

---
+++ client/src/components/TeacherFlowsList.jsx
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Modal,
+  Button,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+import BuildSetUp from './BuildSetUp.jsx';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  overflow: 'scroll'
+};
+
+const TeacherFlowsList = () => {
+  const [teachers, setTeachers] = useState([]);
+  const [expanded, setExpanded] = useState(false);
+  const [flows, setFlows] = useState([]);
+  const [savedFlow, setSavedFlow] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState(null);
+  const [favorites, setFavorites] = useState([]);
+
+  const addOrRemoveFavorite = () => {
+    if (favorites.map(flow => flow.name).includes(name)) {
+      axios.delete(`/favorites/${name}`)
+        .then(() => {
+          getFavorites();
+        })
+        .catch(err => {
+          console.warn(err);
+        })
+
+    } else {
+      axios.post('/favorites/', { data: { name }})
+        .then(() => {
+          getFavorites();
+        })
+        .catch(err => {
+          console.warn(err);
+        })
+    }
+  }
+
+  const getFavorites = () => {
+    axios.get('/favorites/')
+      .then(({ data }) => {
+        setFavorites(data);
+      })
+      .catch(err => {
+        console.warn(err);
+      })
+  }
+
+  const renderBuiltFlow = (flow) => {
+    axios.get(`/flow/getSavedFlow/${flow.id}`)
+      .then(({ data }) => {
+        setName(flow.name);
+        setSavedFlow(data);
+        setOpen(true);
+      })
+      .catch(err => {
+        console.warn(err);
+      })
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setExpanded(false);
+    setFlows([]);
+  }
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+    getTeacherFlows(teachers[panel]);
+  }
+
+  const getTeachers = () => {
+    axios.get('/teachers/')
+      .then(({ data }) => {
+        setTeachers(data);
+        getFavorites();
+      })
+      .catch(err => {
+        console.warn(err);
+      })
+  };
+
+  const getTeacherFlows = (teacher) => {
+    axios.get(`/teachers/${teacher.id}`)
+      .then(({ data }) => {
+        setFlows(data);
+      })
+      .catch(err => {
+        console.warn(err);
+      })
+  }
+
+  useEffect(() => {
+    getTeachers();
+  }, []);
+
+  return (
+    <Box sx={{width: '50%', marginLeft: '20px'}}>
+      {
+        teachers.map((teacher, i) => (
+          <Accordion expanded={expanded === i} onChange={handleChange(i)} key={i}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel${i}bh-content`}
+              id={`panel${i}bh-header`}
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                {teacher.full_name}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {
+                  savedFlow.length ?
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="parent-modal-title"
+                    aria-describedby="parent-modal-description"
+                  >
+                  <Box sx={{ ...style, width: '90%', height: '90%' }}>
+                    {
+                      name ?
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        m="auto"
+                      ><Typography><h2 id="parent-modal-title" style={{ paddingRight: '5px'}}>{name}</h2></Typography>
+                      <Button onClick={addOrRemoveFavorite}>
+                        {
+                          favorites.map(flow => flow.name).includes(name) ? <FavoriteIcon /> : <FavoriteBorderIcon />
+                        }
+                      </Button>
+                      </Box>
+                      : null
+                    }
+                    <BuildSetUp jobBodyParts={[]} video={false} savedFlow={savedFlow} />
+                  </Box>
+                  </Modal>
+                  : <>{flows.map((flow, i) => <Button onClick={() => renderBuiltFlow(flow)} key={i}><div>{flow.name}</div></Button>)}</>
+                }
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))
+      }
+    </Box>
+  )
+}
+
+export default TeacherFlowsList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/routes/Profile.js
@@ -0,0 +1,149 @@
+const { Router } = require('express');
+
+const Poses = Router();
+
+const { UserPose, Pose, Flow, Following, User } = require('../db/sequelize');
+/*
+knows pose list: select all rows from table user_poses where userPoses.userId === req.user.id && userPoses.pose_rank === 1. -----> for each row returned, find row from Poses tables where Poses.id === returned row.poseId
+
+10:50
+working on pose list: select all rows from table userPoses where userPoses.userId === req.user.id && userPoses.pose_rank === 0. -----> for each row returned, find row from Poses tables where Poses.id === returned row.poseId
+*/
+
+// Get all poses
+Poses.get('/', (req, res) => {
+  UserPose.findAll().then((data) => {
+    res.status(200).send(data);
+  })
+    .catch(() => {
+      res.status(404).send('Error!');
+    });
+});
+
+// UserPose table request checks if user_id matches pose_id
+Poses.get('/userPosesWorkingOn', (req, res) => {
+  const { id } = req.user.dataValues;
+
+  UserPose.findAll({ where: {userId: id, pose_rank: 0 }})
+    .then(async (response) =>  {
+      Promise.all(response.map(row => Pose.findByPk(row.poseId)))
+        .then(poses => {
+          res.status(200).send(poses);
+        })
+        .catch(err => {
+          console.warn(err);
+        })
+    }).catch((err) => {
+      console.warn("userPose:", err);
+    })
+})
+
+Poses.get('/userPosesKnown', (req, res) => {
+  const { id } = req.user.dataValues;
+
+  UserPose.findAll({ where: {userId: id, pose_rank: 1 }})
+    .then(async (response) =>  {
+      Promise.all(response.map(row => Pose.findByPk(row.poseId)))
+        .then(poses => {
+          res.status(200).send(poses);
+        })
+        .catch(err => {
+          console.warn(err);
+        })
+    }).catch((err) => {
+      console.warn("userPose:", err);
+    });
+})
+
+//Get all Poses from Pose table
+Poses.get('/allPoses', (req, res) => {
+  Pose.findAll().then((data) => {
+    res.status(200).send(data);
+  })
+    .catch(() => {
+      res.status(404).send('Error!');
+    });
+});
+
+// post request for user_pose
+Poses.post('/userPoses', (req, res) => {
+  // console.log('req.body:', req.body.data);
+  // console.log('user:', req.user.dataValues.id);
+
+  UserPose.create({ pose_rank: 1, userId: req.user.dataValues.id, poseId: req.body.data.id })
+    .then((data) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.warn(err);
+      res.status(404).send('Error!');
+    });
+});
+
+// create poses that user is still working on
+Poses.post('/userPosesDontKnow', (req, res) => {
+  const { selectedOptions } = req.body;
+  UserPose.create({ poseId: req.body.data.id, pose_rank: 0, userId: req.user.dataValues.id })
+    .then((data) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.warn(err);
+      res.status(404).send('Error!');
+    });
+});
+
+// Get all the users' saved
+Poses.get('/savedFlows', (req, res) => {
+  const { id } = req.user.dataValues;
+
+  Flow.findAll({ where: { userId: id }}).then((data) => {
+    res.status(200).send(data);
+  })
+    .catch(() => {
+      res.status(404).send('Error!');
+    });
+});
+
+Poses.get('/sharedFlows', (req, res) => {
+  const { id } = req.user.dataValues;
+
+  Following.findAll({ where: { follower_id: id }})
+    .then(rows => {
+      Promise.all(rows.map(row => Flow.findAll({ where: { userId: row.followee_id, is_public: true }})))
+        .then(flows => {
+          res.status(200).send(flows.flat());
+        })
+        .catch(err => {
+          console.warn(err);
+          res.sendStatus(404);
+        })
+    })
+    .catch(err => {
+      console.warn(err);
+      res.sendStatus(404);
+    })
+});
+
+Poses.put('/changeTeacherStatus', (req, res) => {
+  const { id } = req.user.dataValues;
+  const { is_teacher } = req.body.data;
+
+  User.update({ is_teacher }, {
+    where: {
+      id
+    }
+  })
+  .then(() => {
+    res.sendStatus(201);
+  })
+  .catch(err => {
+    console.warn(err);
+    res.sendStatus(404);
+  })
+})
+
+// export the api calls
+module.exports = {
+  Poses,
+};
