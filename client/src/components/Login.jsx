291900dee2612278741f30064343acf748fa06a5
emmapejko <emma.pejko@gmail.com>
2021-11-03 16:54:24 -0500

    fix: removing unused code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import styled from 'styled-components';
 
 import Chakras from './Chakras.jsx';
-
-const BackgroungImg = styled.img`
-    display: block;
-    width: 100%;
-    height: 100%;
-`;
 
 function Login({ style }) {
   return (
