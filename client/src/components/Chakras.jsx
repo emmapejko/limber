6e562bdd7122eb3af6b153e361683f50f8e4c627
emmapejko <emma.pejko@gmail.com>
2021-11-04 11:17:56 -0500

    fix: remove unused code for login

--- client/src/components/Chakras.jsx
+++ client/src/components/Chakras.jsx
@@ -3,7 +3,6 @@ import {
   Box
 } from '@mui/material';
 
-//import skelly from '../images/skellyton.png';
 import skelly from '../images/skellyfire.jpg';
 
 const Chakras = () => {
@@ -16,22 +15,7 @@ const Chakras = () => {
       style={{maxWidth: '401px'}}
     >
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 401 622">
-      <image width="401" height="622" xlinkHref={skelly}></image>
-      {/* <a xlinkHref="crown">
-        <circle cx="198" cy="15" r="15" opacity="50%" fill="#d55df0" />
-      </a><a xlinkHref="third eye">
-        <circle cx="198" cy="50" r="15" opacity="50%" fill="#4481ee" />
-      </a><a xlinkHref="throat">
-        <circle cx="195" cy="93" r="15" opacity="50%"fill="#3ad3d2" />
-      </a><a xlinkHref="heart">
-        <circle cx="195" cy="155" r="15" opacity="50%" fill="#8fd33b" />
-      </a><a xlinkHref="solar plexus">
-        <circle cx="195" cy="200" r="15" opacity="50%" fill="#ffba39" />
-      </a><a xlinkHref="sacral">
-        <circle cx="195" cy="245" r="15" opacity="50%" fill="#ff862e" />
-      </a><a xlinkHref="root">
-        <circle cx="195" cy="290" r="15" opacity="50%" fill="#ff4e53" />
-      </a> */}
+      <image width="401" height="622" xlinkHref={skelly} />
     </svg>
     </Box>
   )
